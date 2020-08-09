510271e2a8b441cc5e6445acd02f61d9f08ee828
dedshit <62318734+dedshit@users.noreply.github.com>
2020-08-09 22:55:20 +0530

    ✅

--- viewbot.py
+++ viewbot.py
@@ -2,11 +2,10 @@
 import random
 from bs4 import BeautifulSoup as pi
 import requests as R
-
 class test:
     def __init__(self, proxy, url):
         self.proxy = proxy
-        self.url = url
+        self.url = url      
     def views(self):
         Z = R.get(self.url)
         X = pi(Z.content.decode('utf-8'), 'html5lib')
@@ -18,7 +17,7 @@ class test:
         b5 = a2[0]
         bg = b5.replace("\\", "")
         bn = bg[13:-1]
-        print("\n Current Views: ", bn)
+        print("\n Current Views: ", bn)       
     def test_proxy(self):
         a = R.get(self.proxy)
         b = a.content.decode('utf-8')
@@ -34,16 +33,25 @@ class test:
             'Mozilla/5.0 (Linux; Android 6.0; CAM-L21 Build/HUAWEICAM-L21; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/62.0.3202.84 Mobile Safari/537.36',
             'Mozilla/5.0 (Linux; U; Android 4.2.2; de-de; Lenovo A7600-F Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)'
-        ]
+        ]    
         for i in b.split(','):
             try:
                 while True:
                     for o in range(1, 10):
                         U = random.choice(p)
-                    h = {"User-Agent":U}
+                    h = {
+                        "User-Agent":U,
+                        "Accept":"*/*",
+                        "Accept-Encoding":"gzip, deflate, br",
+                        "Accept-Language":"en-us,en;q=0.5",
+                        "Connection":"keep-alive",
+                        "Host":"www.youtube.com",
+                        "Referer":"https://www.youtube.com/",
+                        "TE":"Trailers"    
+                    }
                     print("--------")
                     try:
-                        rr = R.get(self.url, headers=h)
+                        rr = R.get(self.url, headers=h, allow_redirects=False)
                         if rr.status_code == 200:
                             print("Ok")
                     except R.exceptions.ConnectionError:
@@ -53,7 +61,7 @@ class test:
             except R.exceptions.ChunkedEncodingError:
                 print('connection error!')
             except KeyboardInterrupt:
-                print('Quitting..')
+                print('Quitting..')               
 q = input('\n Enter video url : ')
 q1 = test("https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=2000&country=all&ssl=yes&anonymity=elite", q)
 q1.test_proxy()
